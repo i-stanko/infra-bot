7abb1b6414e7926396b588003277ec66d7cc81db
Ivan Stanko <i.stanko86@gmail.com>
2026-01-05 09:37:44 +0200

    init infra-bot telegram integration (telebot v4)

--- cmd/start.go
+++ cmd/start.go
@@ -1,39 +1,41 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v4"
 )
 
-// startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Start Telegram bot",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("start called")
+
+		token := os.Getenv("TELE_TOKEN")
+		if token == "" {
+			log.Fatal("TELE_TOKEN is not set")
+		}
+
+		bot, err := telebot.NewBot(telebot.Settings{
+			Token:  token,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		bot.Handle(telebot.OnText, func(c telebot.Context) error {
+			return c.Send("👋 Hi! I'm KVN.")
+		})
+
+		log.Println("infra-bot started")
+		bot.Start()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
